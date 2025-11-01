9308c3841a588e424c300eeaa38b317b3967bc8c
What's Up Beijing 因果 (Yīnguǒ) <67036154+0n4u@users.noreply.github.com>
2025-11-01 12:44:26 -0500

    Delete vrc_log directory
    remade

--- vrc_log/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-__version__ = "4.5.5"
-USER_AGENT = f"VRCLogX/{__version__}"
-logger = logging.getLogger(__name__)
--- vrc_log/exceptions.py
+++
@@ -1,6 +0,0 @@
-class RPCValidationError(Exception):
-    """Raised when Discord RPC payload validation fails"""
-
-
-class AvatarMetadataError(Exception):
-    """Raised for invalid avatar metadata"""
